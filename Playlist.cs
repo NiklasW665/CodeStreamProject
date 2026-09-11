d689c9c303e10b86a4db3868452efe53cfab20e3
Wilson-sweetie <wilsonsweeti3@gmail.com>
2026-09-11 07:47:47 +0200

    fix some of the file storing issues and added the functionality in Playlist.cs and DataManager.cs

--- Playlist.cs
+++ Playlist.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using CodeStream20.Playlists;
 
 namespace CodeStream20
 {
@@ -17,7 +18,11 @@ namespace CodeStream20
         public TimeSpan Duration { get; set; }
         public string FilePath { get; set; }
         //songs inside the playlist
-        public List<string> Songs { get; set; } 
+        public List<Song> Songs { get; set; } //Tamia- i created class Song.cs to hold more detailed song information, but for now, we will keep the list of songs as a list of strings.
+        //i also added the owner var to see who created the playlist, but we can remove it if we want to keep the playlist more generic
+        public string Owner { get; set; }
+        //true or false var if user what others to see their playlist or not
+        public bool IsShared { get; set; }
 
         //constructor to easily insantiate a new playlist object
         public Playlist(string title, string artist, string genre, TimeSpan duration, string filePath)
@@ -28,13 +33,17 @@ namespace CodeStream20
             Duration = duration;
             FilePath = filePath;
             //create a new empty list of songs
-            Songs = new List<string>();
+            Songs = new List<Song>();//update this to List<Song> instead of List<string> to hold more detailed song information
         }
 
         //Method to add a song to the playlist
-        public void AddSong(string songTitle)
+        public void AddSong(Song songTitle)//changed the parameter type from string to Song to hold more detailed song information
         {
             Songs.Add(songTitle);
+        }
+        public void RemoveSong(Song songTitle)//changed the parameter type from string to Song to hold more detailed song information
+        {
+            Songs.Remove(songTitle);
         }
     }
 }
--- Playlists/Song.cs
+++ Playlists/Song.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CodeStream20.Playlists
 {
     //this class represents a song object with properties for title, artist, album, genre, and file path.
     //Playlists now hold list<Song> instead of list<string> for better data management and to allow for more detailed song information.
     [Serializable]
-    internal class Song
+    public class Song
     {
         //Song Individual properties
-        public string Title { get; set; } = string.Empty;
-        public string Artist { get; set; } = string.Empty;
-        public string Album { get; set; } = string.Empty;
-        public string Genre { get; set; } = string.Empty;
-        public string FilePath { get; set; } = string.Empty;
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public string FilePath { get; set; }
 
         //Default Constructor to easily instantiate a new song object
         public Song() { }
